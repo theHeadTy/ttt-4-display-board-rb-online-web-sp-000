c510b01d9c9eb6505c3ba69ba82af218124008b6
generic-puppet-7755 <tasmith1990@gmail.com>
2019-10-03 16:20:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,11 +3,11 @@
 
 def display_board(board = Array.new(9, " "))
   
-  puts "#{board[0]}|#{board[1]}|#{board[3]}"
+  puts "#{board[0]}|#{board[1]}|#{board[2]}"
   puts "---------"
-  puts "#{board[4]}|#{board[5]}|#{board[6]}"
+  puts "#{board[3]}|#{board[4]}|#{board[5]}"
   puts "---------"
-  puts "#{board[7]}|#{board[8]}|#{board[9]}"
+  puts "#{board[6]}|#{board[7]}|#{board[8]}"
   puts "---------"
   
   puts "\n"
